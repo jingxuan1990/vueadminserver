c470761495c8810b56914ba4fc8cb27a06e94eee
ShiQianfeng720 <sqflwh@163.com>
2019-02-23 17:59:43 +0800

    初始化成功

---
+++ conf/mysqlConf.js
@@ -0,0 +1,9 @@
+module.exports = {
+    mysql: {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database:'express-mysql-demo',
+        connectionLimit: 10
+    }
+};
