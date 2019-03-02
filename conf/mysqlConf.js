a3a112436f3c1d572a1da1132143d6b570358366
ShiQianfeng720 <sqflwh@163.com>
2019-03-02 12:07:04 +0800

    清理服务端

--- conf/mysqlConf.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    mysql: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database:'spa',
-        connectionLimit: 10
-    }
-};
--- dao/userSqlMap.js
+++
@@ -1,9 +0,0 @@
-var userSqlMap = {
-    add: 'INSERT INTO user(name,nick_name,birth,detail_address,sex)VALUES(?,?,?,?,?)',
-    deleteById: 'delete from user where id = ?',
-    update: 'update user set username=?, password=? where id=?',
-    list: 'select * from user',
-    getById: 'select * from user where id = ?'
-};
-
-module.exports = userSqlMap;
