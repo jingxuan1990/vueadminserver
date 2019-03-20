783bc143a1912fd575f90aa74980475e16c3bbfa
ShiQianfeng720 <sqflwh@163.com>
2019-03-21 00:03:54 +0800

    提交代码

--- routes/admin.js
+++ routes/admin.js
@@ -13,9 +13,79 @@ router.get('/', function (req, res) {
         if (err) {
             throw err;
         }
-        res.send({code: 20000, data: result, msg: "查询成功"});
+
+        let newList = [];
+        for (let current of result) {
+            if (current.gmt_create) {
+                current.gmt_create = moment(current.gmt_create).format("YYYY-MM-DD HH:mm:ss");
+            }
+
+            if (current.gmt_modified) {
+                current.gmt_modified = moment(current.gmt_modified).format("YYYY-MM-DD HH:mm:ss");
+            }
+
+
+            newList.push(current);
+        }
+
+        res.send({code: 20000, data: newList, msg: "查询成功"});
     });
 });
 
+
+/**
+ * 创建管理员
+ */
+router.post('/', function (req, res) {
+    const username = req.body['username'];
+    const password = req.body['username'];
+    const nick_name = req.body['nick_name'];
+    const roleJson = JSON.stringify(req.body['roles']);
+    const gmt_create = moment().format("YYYY-MM-DD HH:mm:ss");
+
+    const insertSql = 'insert into admin_user (username, password, nick_name, role, gmt_create, gmt_modified)' +
+        'values (?, ?, ? ,? ,? ,?)';
+    pool.query(insertSql, [username, password, nick_name, roleJson, gmt_create, gmt_create],
+        (err, result) => {
+            if (err) {
+                throw err;
+            }
+            res.send({code: 20000, data: result, msg: "创建管理员成功"});
+        });
+});
+
+/**
+ * 删除管理员
+ */
+router.delete('/:id', function (req, res) {
+    const id = parseInt(req.params['id']);
+    const delSql = 'delete from admin_user  where id = ?';
+    pool.query(delSql, [id], (err, result) => {
+        if (err) {
+            throw err;
+        }
+        res.send({code: 20000, data: result, msg: "删除成功"});
+    });
+});
+
+/**
+ * 修改管理员
+ */
+router.put('/', function (req, res) {
+    const id = parseInt(req.body['id']);
+    const username = req.body['username'];
+    const password = req.body['username'];
+    const nick_name = req.body['nick_name'];
+    const roleJson = JSON.stringify(req.body['roles']);
+    let gmt_modified = moment().format("YYYY-MM-DD HH:mm:ss")
+
+    let sql = 'update admin_user set username = ?, nick_name=?,password=?, role=?, gmt_modified=? where id = ?'
+    pool.query(sql, [username, nick_name, password, roleJson, gmt_modified, id], (err, result) => {
+        if (err) {
+            throw err;
+        }
+        res.send({code: 20000, data: result, msg: "修改管理员成功"});
+    });
+});
 
 module.exports = router;
--- routes/suite.js
+++ routes/suite.js
@@ -19,4 +19,66 @@ router.get('/', function (req, res) {
 });
 
 
+/**
+ * 创建套间
+ */
+router.post('/', function (req, res) {
+    // 获取请求参数
+    const name = req.body['name'];
+    const status = parseInt(req.body['status']);
+    const type = req.body['type'];
+
+    // 自动生成noid
+    const noId = 'N' + randomNum(100, 999);
+    // 当前时间
+    let gmtCreate = moment().format("YYYY-MM-DD HH:mm:ss");
+
+    const sql = 'insert into suite (noid, name, status, type, gmt_create, gmt_modified)' +
+        ' values(?, ?, ?, ?, ?, ?)'
+    pool.query(sql, [noId, name, status, type, gmtCreate, gmtCreate], (err, result) => {
+        if (err) {
+            throw err;
+        }
+        res.send({code: 20000, data: result, msg: "创建成功"});
+    });
+});
+
+//生成从minNum到maxNum的随机数
+function randomNum(minNum, maxNum) {
+    switch (arguments.length) {
+        case 1:
+            return parseInt(Math.random() * minNum + 1, 10);
+            break;
+        case 2:
+            return parseInt(Math.random() * (maxNum - minNum + 1) + minNum, 10);
+            break;
+        default:
+            return 0;
+            break;
+    }
+}
+
+
+
+/**
+ * 修改套间
+ */
+router.put('/', function (req, res) {
+    // 获取请求参数
+    console.log('body'  + JSON.stringify(req.body))
+
+    const suiteId = req.body['id']; // 套间ID
+    const remark = req.body['remark']; // 备注
+    const userNum = req.body['use_num']; // 使用人数
+    const phone = req.body['phone']; // 预约人手机号
+    const username = req.body['username']; // 预约人姓名
+    const bookingSartTime = req.body['booking_sart_time']; // 预约开始时间
+    const bookingEndTime = req.body['booking_end_time']; // 预约结束时间
+    const zhanStartTime = req.body['zhan_start_time']; // 占用开始时间
+    const zhanEndTime = req.body['booking_end_time']; // 占用结束时间
+    const status = req.body['status']; // 套间状态
+
+});
+
+
 module.exports = router;
